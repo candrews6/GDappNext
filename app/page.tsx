5ae044a8d90ba39a5681d9fef79cd8731bf91f71
candrews6 <charlie.andrews6@mail.com>
2024-02-15 20:17:45 +0000

    reset 11

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (<div><h5 className="px-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
+    {/* button(onclick="myFunction()" id="myBtn")="Read more" */}
+    <div data-aos="fade-up" className="grid grid-cols-1 lg:grid-cols-2 gap-0 px-14" contextMenu="return false">
+      <Link href="/floofs">
+        <img src="images/HomepageHeaderImage.jpg" alt='Header image'></img>
+      </Link>
+      <img data-aos="fade-up" src="images/alt3.png" alt='test2'></img>
+      <img data-aos="fade-up" src="images/alt3.png" alt="test3"></img>
+      <img data-aos="fade-up" src="images/alt3.png" alt="test4"></img>
+      <img data-aos="fade-up" src="images/alt3.png" alt="test5"></img>
+      <img data-aos="fade-up" src="images/alt3.png" alt="test6"></img>
+      <img data-aos="fade-up" src="images/alt3.png" alt="test7"></img>
+      <img data-aos="fade-up" src="images/alt3.png" alt="test8"></img>
+    </div >
+  </div>);
+}
