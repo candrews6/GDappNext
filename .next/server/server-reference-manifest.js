47c85bfa0bf30cde0e674c903fa80ef2f1f8e347
candrews6 <charlie.andrews6@mail.com>
2024-02-15 19:55:58 +0000

    git reset 8

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"nQsNN5uR3wPA+g5S5cLiu252mQYW1bBonrxsUxsdQ0M=\"}"
-=======
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"xqwrNYf+gXDc2nYXwMaoIZ6i4PNeauR+y1X1eMzCr2M=\"}"
->>>>>>> 672a6e4177052b18ff652242e35ae01f78689ed0
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"UzrDJzg1A8jmH1L+DEHUyU/KEhydlRdUxhdEA+LD3gQ=\"}"
