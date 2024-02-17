263387158c39f6c53385a192a37743769faa2ad3
candrews6 <charlie.andrews6@mail.com>
2024-02-17 15:02:03 +0000

    add hover over and responsive viewport

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default function Page() {
   return (<div>
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-0 px-14 py-10" contextMenu="return false">
       <Link href="/floofs">
-        {/* <figure className="inline-block overflow-hidden relative group">
+        <figure className="inline-block overflow-hidden relative group">
+          <img data-aos="fade-up" className="image" src="images/HomepageHeaderImage.jpg" alt='Header image'></img>
           <div className="absolute inset-0 text-white bg-black bg-opacity-50 flex items-center justify-center transition-all duration-300 text-2xl opacity-0 group-hover:opacity-100">
-          <strong className="relative transition-all duration-300 top-4 group-hover:top-0">
-          Here is some text!
-          </strong>
+            <strong className="relative transition-all duration-300 top-4 group-hover:top-0">
+              Here is some text!
+            </strong>
           </div>
-        </figure> */}
-        <img data-aos="fade-up" src="images/HomepageHeaderImage.jpg" alt='Header image'></img>
+        </figure>
       </Link>
       <img data-aos="fade-up" src="images/alt3.png" alt='test2'></img>
       <img data-aos="fade-up" src="images/alt3.png" alt="test3"></img>
